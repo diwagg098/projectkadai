824c900fb7a409b82c60d17273dd1e7ab04441b9
diwarunna <diwagg098@gmail.com>
2021-10-06 21:48:24 +0700

    add database

--- database/migrations/2021_09_25_162139_create_product_table.php
+++ database/migrations/2021_09_25_162139_create_product_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreatePasswordResetsTable extends Migration
+class CreateProductTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,12 @@ class CreatePasswordResetsTable extends Migration
      */
     public function up()
     {
-        Schema::create('password_resets', function (Blueprint $table) {
-            $table->string('email')->index();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
+        Schema::create('product', function (Blueprint $table) {
+            $table->string('id_product')->primary();
+            $table->string('id_category');
+            $table->string('nama_product');
+            $table->integer('harga');
+            $table->timestamps();
         });
     }
 
@@ -27,6 +29,6 @@ class CreatePasswordResetsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('password_resets');
+        Schema::dropIfExists('product');
     }
 }
